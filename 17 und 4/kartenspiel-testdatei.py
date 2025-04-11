2cc08b5bea1da69716dac7a8daa35e28a7b787a0
Hagen <hagenotto23@icloud.com>
2025-04-11 08:43:16 +0200

    Funktioniert

--- 17 und 4/kartenspiel-testdatei.py
+++ 17 und 4/kartenspiel-testdatei.py
@@ -1,18 +1,31 @@
 from kartenspiel import *
 # Testprogramm
-
+spiel = True
 kartenstapel = Kartenstapel()
-
 kartenstapel.mischen()
-print(kartenstapel.kartenListe)
-print()
 kartenhaufen = Kartenhaufen()
 gezogeneKarte = kartenstapel.karteZiehen()
-kartenhaufen.hinzufuegen(gezogeneKarte)
-print(kartenhaufen.kartenListe)
-print(kartenstapel.kartenListe)
-print(kartenhaufen.wert)
 
+
+print("___________Blackjack_____________")
+if input("Möchtest du spielen? (Ja/Nein)\n") == "Ja":
+    print("Hier sind deine Zwei Karten")
+    for i in range(2):
+        gezogeneKarte = kartenstapel.karteZiehen()
+        kartenhaufen.hinzufuegen(gezogeneKarte)
+    print(kartenhaufen.kartenListe)
+    print(kartenhaufen.wert)
+    while spiel:
+        if input("Möchtest du eine weitere ziehen oder bei zwei bleiben ? (Hit/Stay)\n") == "Hit":
+            gezogeneKarte = kartenstapel.karteZiehen()
+            kartenhaufen.hinzufuegen(gezogeneKarte)
+            print(kartenhaufen.kartenListe)
+            print(kartenhaufen.wert)
+            if kartenhaufen.wert > 21:
+                print("Du Hast leider Verloren.")
+                spiel = False
+        else:
+            Spiel = False
 """
 Schreiben Sie ein Testprogramm, dass Sie 17 und 4 spielen lässt. Nutzereingaben
 sollen darüber entscheiden, ob Sie noch eine Karte ziehen oder nicht und benutzer-
